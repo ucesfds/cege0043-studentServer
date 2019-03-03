251fcee192325bdfb1dbc2187f8940a9e5c83186
Francisco Salgado <ucesfds@ucl.ac.uk>
2019-03-03 13:54:31 -0500

    Added cross origin request functionality

--- studentServer.js
+++ studentServer.js
@@ -2,6 +2,13 @@
 var express = require('express');
 var path = require("path");
 var app = express();
+
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "X-Requested-With");
+next();
+});
+
 // add an http server to serve files to the Edge browser
 // due to certificate issues it rejects the https files if they are not
 // directly called in a typed URL
